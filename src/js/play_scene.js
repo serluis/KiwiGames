fa4165f9f8a77025dfa30246461c8c2c887440de
Sergio Luis <serluis@ucm.es>
2018-11-16 12:37:58 +0100

    cambiados los terminos del html
    ahora se muestran dos imagenes de prueba ke se mueven en dif direcciones

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -3,25 +3,43 @@
   var PlayScene = {
   	preload: function(){
   		this.game.load.image('prueba', 'images/prueba.jpg');
-  	};
-  create: function () {
-    var logo = this.game.add.sprite(
-      this.game.world.centerX, this.game.world.centerY, 'prueba');
+  	},
+    create: function () {
+   // var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'prueba');
+    var logo = this.game.add.existing(new Entity(this.game, 10, 10, 'prueba'));
+
+    this.game.add.existing(new Enemy(this.game, 10, 50, 'prueba'));
+
     logo.anchor.setTo(0.5, 0.5);
   }
-};
+}
 
 module.exports = PlayScene;
 var Entity = function (game,x,y){
-  sprite.call(this,game,x,y);
+  Phaser.Sprite.call(this,game,x,y,'prueba');
 }
+
+Entity.prototype= Object.create(Phaser.Sprite.prototype);
+Entity.prototype.constructor=Entity;
+
+Entity.prototype.update = function() {
+  this.x++;
+}
+
+
 var Enemy = function (game,x,y){
   Entity.call(this,game,x,y)
 }
-//Enemy.prototype= object.create(Entity.prototype);
+Enemy.prototype= Object.create(Entity.prototype);
 Enemy.prototype.constructor=Enemy;
 
-var cosa = function(cosa1,x,y){//esto es una constructora
+Enemy.prototype.update = function() {
+  Entity.prototype.update.apply(this);
+  this.y++;
+}
+
+
+/*var cosa = function(cosa1,x,y){//esto es una constructora
 	sprite.call(this,cosa1,x,y);
 	//colocar
 	//rotacion
@@ -32,4 +50,4 @@ var cosa = function(cosa1,x,y){//esto es una constructora
 }
 //justo despues se pone las cosas de prototypes
 //cosa.prototype.update=function (){}//aqui se hace el update
-//para usar las variables de cosa se pone this.variable=3;
+//para usar las variables de cosa se pone this.variable=3;*/
