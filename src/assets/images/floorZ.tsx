45797df61b0c8e10448b4165e30a5e59c9759887
Sergio Luis <serluis@ucm.es>
2018-11-17 21:43:27 +0100

    intento de boton

---
+++ src/assets/images/floorZ.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="floorZ" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="KiwiGames/src/assets/images/floorZ.png" width="256" height="256"/>
+</tileset>
---
+++ src/assets/images/walls-128x128.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="walls-128x128" tilewidth="128" tileheight="128" tilecount="64" columns="8">
+ <image source="KiwiGames/src/assets/images/walls-128x128.png" width="1024" height="1024"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="5" x="1" y="0" width="126" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
