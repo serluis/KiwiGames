0a01a218bd46a15ae2f0e102d1b5a0f46f02417e
Sergio Luis <serluis@ucm.es>
2018-11-20 15:05:26 +0100

    los menus empiezan a formarse

--- src/js/MainMenu.js
+++ src/js/MainMenu.js
@@ -1,25 +1,26 @@
-'use strict';
-var PlayScene = require('./play_scene.js');
+'use strict'
+//var PlayScene = require('./play_scene.js');
+
 var Menu = {
 	preload: function(){
     this.game.load.image('menu','./assets/images/menu.png');
-    this.game.load.audio('musicaFondo','../assets/sounds/ZombieRock.mp3');
+    var ZombieRock = this.game.load.audio('musicaFondo','../assets/sounds/ZombieRock.mp3');
     this.game.load.image('playbutton','../assets/images/playbutton.png');
   },
   
   create: function() {
    var fondo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'menu');
-   this.game.add.audio('musicaFondo').loopFull(1);
+   this.game.add.audio('musicaFondo'/*ZombieRock*/).loopFull(1);
    fondo.anchor.setTo(0.5, 0.5);
-   var playbutton = new Button(game, 400, 300,'playbutton', actionOnClick,/* this,0,0,0,0*/);
-   playbutton.anchor.setTo(0.5,0.3);
+   //var playbutton = new Button(game, 400, 300,'playbutton', actionOnClick,/* this,0,0,0,0);
+   
   },
-  function actionOnClick () {
+  /*function actionOnClick () {
 
     console.log('button clicked');
     game.state.start('play');
 
-}
+}*/
   
    	
    
